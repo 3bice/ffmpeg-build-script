98ed11fcb096b3747724367586bab3a0ae6c91f8
3bice <151855671+3bice@users.noreply.github.com>
2024-12-20 14:59:52 +0800

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,113 @@
+use std::{
+    env::{self},
+    path::PathBuf,
+    process::{exit, Command},
+};
+
+fn main() {
+    let current_dir = match env::current_dir() {
+        Ok(current_dir) => current_dir,
+        Err(e) => {
+            eprintln!("Failed to get current directory: {}", e);
+            exit(1);
+        }
+    };
+
+    println!("current dir: {:?}", current_dir);
+    let dir_content = std::fs::read_dir(current_dir).unwrap();
+
+    for entry in dir_content {
+        let entry = entry.unwrap();
+        let file_path = entry.path();
+
+        println!("file_path: {:?}", file_path);
+        let output = Command::new("otool").arg("-L").arg(&file_path).output();
+
+        let out = match output {
+            Ok(output) => {
+                if output.status.success() {
+                    // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
+                    String::from_utf8_lossy(&output.stdout).to_string()
+                } else {
+                    eprintln!("Command failed with status: {}", output.status);
+                    eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
+                    exit(1);
+                }
+            }
+            Err(e) => {
+                eprintln!("Failed to execute command: {}", e);
+                exit(1);
+            }
+        };
+
+        for line in out.lines() {
+            eprintln!("--- {}", line);
+            if !line.trim().starts_with("/usr/local/opt") {
+                continue;
+            }
+            let link_to_lib_path = line.trim().split(" ").next().unwrap();
+            eprintln!("link_to_lib_path = {:?}", link_to_lib_path);
+            let link_to_lib_path_buf = PathBuf::from(link_to_lib_path);
+            let file_name = link_to_lib_path_buf.file_name().unwrap().to_str().unwrap();
+            println!("dest file: {:?}", file_path.join(file_name));
+            if !file_path.parent().unwrap().join(file_name).exists() {
+                std::fs::copy(
+                    link_to_lib_path,
+                    file_path.parent().unwrap().join(file_name),
+                )
+                .unwrap();
+            }
+
+            let origin_lib_file_name = file_path.file_name().unwrap().to_str().unwrap();
+            if line.contains(origin_lib_file_name) {
+                let output = Command::new("install_name_tool")
+                    .arg("-id")
+                    .arg(format!("@rpath/{}", origin_lib_file_name)) // @executable_path @loader_path
+                    .arg(origin_lib_file_name)
+                    .output();
+
+                let _out = match output {
+                    Ok(output) => {
+                        if output.status.success() {
+                            // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
+                            String::from_utf8_lossy(&output.stdout).to_string()
+                        } else {
+                            eprintln!("Command failed with status: {}", output.status);
+                            eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
+                            exit(1);
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to execute command: {}", e);
+                        exit(1);
+                    }
+                };
+                continue;
+            }
+
+            let output = Command::new("install_name_tool")
+                .arg("-change")
+                .arg(link_to_lib_path)
+                .arg(format!("@loader_path/{}", file_name)) // @executable_path @loader_path
+                .arg(file_path.to_str().unwrap())
+                .output();
+
+            let _out = match output {
+                Ok(output) => {
+                    if output.status.success() {
+                        // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
+                        String::from_utf8_lossy(&output.stdout).to_string()
+                    } else {
+                        eprintln!("Command failed with status: {}", output.status);
+                        eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
+                        exit(1);
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to execute command: {}", e);
+                    exit(1);
+                }
+            };
+        }
+    }
+}
