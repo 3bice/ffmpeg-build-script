d195c791de3a93b653b58d1a8ef6542519e5d0dc
3bice <151855671+3bice@users.noreply.github.com>
2024-12-22 23:52:32 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,131 @@
 use std::{
     env::{self},
+    ffi::OsStr,
     io::Write,
     path::PathBuf,
     process::{exit, Command},
     str::FromStr,
 };
+
+fn command<T: AsRef<OsStr>>(cmd: &str, args: Vec<T>) -> Result<String, Box<dyn std::error::Error>> {
+    let output = Command::new(cmd).args(args).output();
+
+    let _out = match output {
+        Ok(output) => {
+            if output.status.success() {
+                // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
+                String::from_utf8_lossy(&output.stdout).to_string()
+            } else {
+                eprintln!("Command failed with status: {}", output.status);
+                eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
+                return Err(String::from_utf8_lossy(&output.stderr).into());
+            }
+        }
+        Err(e) => {
+            eprintln!("Failed to execute command: {}", e);
+            return Err(e.into());
+        }
+    };
+    Ok(_out)
+}
+
+fn modify(
+    current_dir: PathBuf,
+    file_path: PathBuf,
+    have_change: &mut bool,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let out = match command("otool", vec!["-L", &file_path.to_str().unwrap()]) {
+        Ok(out) => out,
+        Err(e) => return Err(e),
+    };
+
+    for line in out.lines() {
+        if line.trim().starts_with("@rpath") {
+            let lib_name = line.split(" ").next().unwrap().split("/").last().unwrap();
+            if !current_dir.join(lib_name).exists() {
+                let out = match command(
+                    "find",
+                    vec!["/", "-name", lib_name, "-maxdepth", "5", "2>/dev/null"],
+                ) {
+                    Ok(out) => out,
+                    Err(e) => return Err(e),
+                };
+                eprintln!("find libs = {:?}", out);
+                std::fs::copy(out.lines().next().unwrap(), current_dir.join(lib_name)).unwrap();
+            }
+        }
+        if !line.trim().starts_with("/usr/local/opt")
+            && !line.trim().starts_with("/opt/homebrew")
+            && !line.trim().starts_with("/usr/local/Cellar")
+        {
+            continue;
+        }
+        eprintln!("--- line: {}", line);
+        *have_change = true;
+        let link_to_lib_path = line.trim().split(" ").next().unwrap();
+        eprintln!("link_to_lib_path = {:?}", link_to_lib_path);
+        let link_to_lib_path_buf = PathBuf::from(link_to_lib_path);
+        let file_name = link_to_lib_path_buf.file_name().unwrap().to_str().unwrap();
+        println!("dest file: {:?}", file_path.join(file_name));
+
+        // source file not exists
+        // if !link_to_lib_path_buf.exists() {
+        //     let mut file = std::fs::OpenOptions::new()
+        //         .create(true)
+        //         .append(true)
+        //         .open(current_dir.parent().unwrap().join("result.txt"))
+        //         .unwrap();
+        //     writeln!(
+        //         file,
+        //         "{} -> {}",
+        //         file_path.to_str().unwrap(),
+        //         link_to_lib_path
+        //     )
+        //     .unwrap();
+        //     continue;
+        // }
+
+        // file not in lib folder
+        if !file_path.parent().unwrap().join(file_name).exists() {
+            println!("copy...");
+            std::fs::copy(
+                link_to_lib_path,
+                file_path.parent().unwrap().join(file_name),
+            )?;
+        } else {
+            continue;
+        }
+
+        let origin_lib_file_name = file_path.file_name().unwrap().to_str().unwrap();
+        eprintln!("origin_lib_file_name = {:?}", origin_lib_file_name);
+        if line.contains(origin_lib_file_name) {
+            let _out = match command(
+                "install_name_tool",
+                vec![
+                    "-id",
+                    &format!("@rpath/{}", origin_lib_file_name),
+                    origin_lib_file_name,
+                ],
+            ) {
+                Ok(out) => out,
+                Err(e) => return Err(e),
+            };
+            continue;
+        }
+
+        let _ = command(
+            "install_name_tool",
+            vec![
+                "-change",
+                link_to_lib_path,
+                &format!("@loader_path/{}", file_name),
+                file_path.to_str().unwrap(),
+            ],
+        );
+    }
+
+    Ok(())
+}
 
 fn main() {
     let mut i = 1;
@@ -27,160 +148,19 @@ fn main() {
             let entry = entry.unwrap();
             let file_path = entry.path();
 
-            println!("file_path: {:?}", file_path);
-            let output = Command::new("otool").arg("-L").arg(&file_path).output();
+            println!(
+                "************************ file_path: {:?} ************************",
+                file_path
+            );
 
-            let out = match output {
-                Ok(output) => {
-                    if output.status.success() {
-                        // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
-                        String::from_utf8_lossy(&output.stdout).to_string()
-                    } else {
-                        eprintln!("Command failed with status: {}", output.status);
-                        eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
-                        exit(1);
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Failed to execute command: {}", e);
-                    exit(1);
-                }
-            };
-
-            for line in out.lines() {
-                if line.trim().starts_with("@rpath") {
-                    let lib_name = line.split(" ").next().unwrap().split("/").last().unwrap();
-                    if !current_dir.join(lib_name).exists() {
-                        let output = Command::new("finc")
-                            .args(["/", "-name", lib_name, "-maxdepth", "5", "2>/dev/null"])
-                            .output();
-
-                        let out = match output {
-                            Ok(output) => {
-                                if output.status.success() {
-                                    // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
-                                    String::from_utf8_lossy(&output.stdout).to_string()
-                                } else {
-                                    eprintln!("Command failed with status: {}", output.status);
-                                    eprintln!(
-                                        "Error Output:\n{}",
-                                        String::from_utf8_lossy(&output.stderr)
-                                    );
-                                    exit(1);
-                                }
-                            }
-                            Err(e) => {
-                                eprintln!("Failed to execute command: {}", e);
-                                exit(1);
-                            }
-                        };
-                        eprintln!("find libs = {:?}", out);
-                        std::fs::copy(out.lines().next().unwrap(), current_dir.join(lib_name))
-                            .unwrap();
-                    }
-                }
-                if !line.trim().starts_with("/usr/local/opt")
-                    && !line.trim().starts_with("/opt/homebrew")
-                    && !line.trim().starts_with("/usr/local/Cellar")
-                {
-                    continue;
-                }
-                eprintln!("--- {}", line);
-                have_change = true;
-                let link_to_lib_path = line.trim().split(" ").next().unwrap();
-                eprintln!("link_to_lib_path = {:?}", link_to_lib_path);
-                let link_to_lib_path_buf = PathBuf::from(link_to_lib_path);
-                let file_name = link_to_lib_path_buf.file_name().unwrap().to_str().unwrap();
-                println!("dest file: {:?}", file_path.join(file_name));
-
-                // source file not exists
-                // if !link_to_lib_path_buf.exists() {
-                //     let mut file = std::fs::OpenOptions::new()
-                //         .create(true)
-                //         .append(true)
-                //         .open(current_dir.parent().unwrap().join("result.txt"))
-                //         .unwrap();
-                //     writeln!(
-                //         file,
-                //         "{} -> {}",
-                //         file_path.to_str().unwrap(),
-                //         link_to_lib_path
-                //     )
-                //     .unwrap();
-                //     continue;
-                // }
-
-                // file not in lib folder
-                if !file_path.parent().unwrap().join(file_name).exists() {
-                    println!("copy...");
-                    std::fs::copy(
-                        link_to_lib_path,
-                        file_path.parent().unwrap().join(file_name),
-                    )
-                    .unwrap();
-                } else {
-                    continue;
-                }
-
-                let origin_lib_file_name = file_path.file_name().unwrap().to_str().unwrap();
-                eprintln!("origin_lib_file_name = {:?}", origin_lib_file_name);
-                if line.contains(origin_lib_file_name) {
-                    let output = Command::new("install_name_tool")
-                        .arg("-id")
-                        .arg(format!("@rpath/{}", origin_lib_file_name)) // @executable_path @loader_path
-                        .arg(origin_lib_file_name)
-                        .output();
-
-                    let _out = match output {
-                        Ok(output) => {
-                            if output.status.success() {
-                                // println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
-                                String::from_utf8_lossy(&output.stdout).to_string()
-                            } else {
-                                eprintln!("Command failed with status: {}", output.status);
-                                eprintln!(
-                                    "Error Output:\n{}",
-                                    String::from_utf8_lossy(&output.stderr)
-                                );
-                                exit(1);
-                            }
-                        }
-                        Err(e) => {
-                            eprintln!("Failed to execute command: {}", e);
-                            exit(1);
-                        }
-                    };
-                    continue;
-                }
-
-                let output = Command::new("install_name_tool")
-                    .arg("-change")
-                    .arg(link_to_lib_path)
-                    .arg(format!("@loader_path/{}", file_name)) // @executable_path @loader_path
-                    .arg(file_path.to_str().unwrap())
-                    .output();
-
-                let _out = match output {
-                    Ok(output) => {
-                        if output.status.success() {
-                            println!("Output:\n{}", String::from_utf8_lossy(&output.stdout));
-                            String::from_utf8_lossy(&output.stdout).to_string()
-                        } else {
-                            eprintln!("Command failed with status: {}", output.status);
-                            eprintln!("Error Output:\n{}", String::from_utf8_lossy(&output.stderr));
-                            exit(1);
-                        }
-                    }
-                    Err(e) => {
-                        eprintln!("Failed to execute command: {}", e);
-                        exit(1);
-                    }
-                };
+            match modify(current_dir.clone(), file_path.clone(), &mut have_change) {
+                Ok(_) => continue,
+                Err(e) => println!("x file_path:{}, error: {}", file_path.to_str().unwrap(), e),
             }
-        }
 
-        if !have_change {
-            break;
+            if !have_change {
+                break;
+            }
         }
     }
 }
